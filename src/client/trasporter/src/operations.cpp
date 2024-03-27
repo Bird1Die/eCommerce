e79a70e4bbfa08d4cc00918327ea8d67e520b410
Bird1Die <luca.cola2002@gmail.com>
2024-03-27 10:36:54 +0000

    si

---
+++ src/client/trasporter/src/operations.cpp
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int NewShippingOrder(Context ctx){
+    system("clear");
+    redisReply *reply;
+    char buf[300];
+    snprintf(buf, sizeof(buf), "XADD transporter * operation_id 4 id_transporter %d", ctx.GetId());
+    reply = RedisCommand(ctx.GetRedis(), buf);
+    string rid = reply->str;
+    while(true){
+        reply = RedisCommand(ctx.GetRedis(), "XREAD COUNT 1 BLOCK 5000 STREAMS %s $", rid.c_str());
+        if(reply->elements == 0){continue;}
+        redisReply *elements = (redisReply*) GetFirstEntryElements(reply);
+        string result = elements->element[1]->str;
+        if(!result.compare("3")){return true;}
+        else{return false;}
+    }
+    return false;
+}
+
+vector<Shipping> GetShippings(Context ctx){
+    char buf[500];
+    snprintf(buf, sizeof(buf), "SELECT s.id, u.username, u.shipping_address, i.product, o.quantity, s.shipping_status FROM orders o JOIN customer u ON o.customer = u.id JOIN insertion i ON o.product = i.id JOIN shipping s ON o.id = s.id_order WHERE s.transporter = %d", ctx.GetId());
+    PGresult *result = ctx.GetDB().ExecSQLtuples(buf);
+    vector<Shipping> shippings;
+    for(int i = 0; i < PQntuples(result); i++){
+        int id = atoi(PQgetvalue(result, i, 0));
+        string username = PQgetvalue(result, i, 1);
+        string shipping_address = PQgetvalue(result, i, 2);
+        string product_name = PQgetvalue(result, i, 3);
+        int quantity = atoi(PQgetvalue(result, i, 4));
+        string shipping_status = PQgetvalue(result, i, 5);
+        shippings.push_back(Shipping(id, username, shipping_address, product_name, quantity, shipping_status));
+    }
+    return shippings;
+}
