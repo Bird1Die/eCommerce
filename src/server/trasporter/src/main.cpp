ca56b1fc768ad3bd13a092b507b410b83cd35fde
Beatrice0399 <beatriceippoliti99@gmail.com>
2024-04-05 15:24:39 +0000

    .

--- src/server/trasporter/src/main.cpp
+++ src/server/trasporter/src/main.cpp
@@ -13,7 +13,6 @@ int main(){
     redisContext *redis = redisConnect("localhost", 6379);
     system("clear");
     bool run = true;
-    vector <thread> threads;
     while(run) {
         /*
         int ch = getch(); // Ottieni il carattere premuto
@@ -22,22 +21,36 @@ int main(){
             break;
         }
         */
-        redisReply *reply = (redisReply*) redisCommand(redis, "XREAD COUNT 1 BLOCK 10000 STREAMS transporter $");
+        redisReply *reply = (redisReply*) redisCommand(redis, "XRANGE transporter - + COUNT 1");
         if (reply->elements != 0){
         redisReply *prima_reply = reply->element[0];
-        cout << "Stream name: " << prima_reply->element[0]->str << endl;
-        redisReply *single_entry = prima_reply->element[1]->element[0];
-        string entry_number = single_entry->element[0]->str;
-        string operation = single_entry->element[1]->element[1]->str;
-        cout << "Entry: " << entry_number << ", operationID: " << operation << endl; 
-        thread th(manageThread, operation, reply, redis);
-        threads.push_back(move(th));
+        string id = prima_reply->element[0]->str;
+        cout << id << endl;
+        string operation = prima_reply->element[1]->element[1]->str;
+        cout << "Entry: " << id << ", operationID: " << operation << endl; 
+        cout << "3" << endl;
+        switch (stoi(operation))
+            {
+            case 1: 
+                cout << "Registrazione" << endl;
+                newRegistrationMsg(reply, redis);
+                break;
+            case 2:
+                cout << "Change shipping status" << endl;
+                changeShippingStatusMsg(reply, redis);
+                break;
+            case 3: 
+                cout << "Login" << endl;   
+                loginMsg(reply, redis); 
+                break;
+            case 4:
+                cout << "Request new Shipping" << endl;
+                requestShippingMsg(reply, redis);
+            }
+        redisCommand(redis, "XDEL transporter %s", id.c_str());
         }
     } 
 
-    for (auto& th : threads) {
-        th.join();
-    }
     // endwin();
     return 0;
 }
